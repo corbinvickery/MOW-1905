b069b6583211d841f3800a343c90ea5e8e33fd4c
corbinvickery <45223748+corbinvickery@users.noreply.github.com>
2019-02-13 13:47:45 -0700

    Update pwmrange.py

--- pwmrange.py
+++ pwmrange.py
@@ -11,30 +11,35 @@ GPIO.setmode (GPIO.BCM)         #we are programming the GPIO by BCM pin numbers.
 GPIO.setup(17,GPIO.OUT)           # initialize GPIO19 as an output.
 
 p = GPIO.PWM(17,500)          #GPIO17 as PWM output, with 100Hz frequency
-pwm = 50
+pwm = 0
 p.start(pwm)                              #generate PWM signal with 0 duty cycle
 
-print (pwm)
-time.sleep(1)
+for x in range (99)
 
-t = 3
-while t>0:                               #execute loop
-    t -= 1
-    for x in range (4999):                          #execute loop for 50 times, x being incremented from 0 to 49.
-       pwm += .01
-       p.ChangeDutyCycle(pwm)               #change duty cycle for varying the brightness of LED.
-       time.sleep(0.001)                           #sleep for 10m second
     print (pwm)
-    time.sleep(.5)
-    for x in range (4999):                         #execute loop for 50 times, x being incremented from 0 to 49.
-        pwm -= .01
-        p.ChangeDutyCycle(pwm)        #change duty cycle for changing the brightness of LED.
-        time.sleep(0.001)                          #sleep for 10m second
-    print (pwm)
-    time.sleep(.5)
+    pwm += 1 
+    time.sleep(.1)
 
-print (pwm)
 
-time.sleep(1)
+
+# t = 3
+# while t>0:                               #execute loop
+#     t -= 1
+#     for x in range (4999):                          #execute loop for 50 times, x being incremented from 0 to 49.
+#        pwm += .01
+#        p.ChangeDutyCycle(pwm)               #change duty cycle for varying the brightness of LED.
+#        time.sleep(0.001)                           #sleep for 10m second
+#     print (pwm)
+#     time.sleep(.5)
+#     for x in range (4999):                         #execute loop for 50 times, x being incremented from 0 to 49.
+#         pwm -= .01
+#         p.ChangeDutyCycle(pwm)        #change duty cycle for changing the brightness of LED.
+#         time.sleep(0.001)                          #sleep for 10m second
+#     print (pwm)
+#     time.sleep(.5)
+
+# print (pwm)
+
+# time.sleep(1)
 
 
