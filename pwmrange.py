0d5d0e693c1c398c76c6c7d7375d40ca3b4de433
corbinvickery <45223748+corbinvickery@users.noreply.github.com>
2019-02-13 14:06:43 -0700

    Update pwmrange.py

--- pwmrange.py
+++ pwmrange.py
@@ -11,23 +11,25 @@ GPIO.setmode (GPIO.BCM)         #we are programming the GPIO by BCM pin numbers.
 GPIO.setup(17,GPIO.OUT)           # initialize GPIO19 as an output.
 
 p = GPIO.PWM(17,500)          #GPIO17 as PWM output, with 100Hz frequency
-pwm = 0
-p.start(pwm)                              #generate PWM signal with 0 duty cycle
-time.sleep(1)
-for x in range (100):
+pwm = 75
+print (pwm)
+p.start(pwm)
+p.ChangeDutyCycle(pwm)                              #generate PWM signal with 0 duty cycle
+time.sleep(3)
+for x in range (24):
 
     print (pwm)
     pwm += 1 
     p.ChangeDutyCycle(pwm)
     time.sleep(.1)
 time.sleep(1)
-for x in range (50):
+for x in range (49):
 
     print (pwm)
     pwm -= 1 
     p.ChangeDutyCycle(pwm)
     time.sleep(.1)
-for x in range (25):
+for x in range (24):
 
     print (pwm)
     pwm += 1 
