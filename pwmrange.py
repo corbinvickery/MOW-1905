8a5abae1746f14d623f6d8692ffd1819b3aac462
corbinvickery <45223748+corbinvickery@users.noreply.github.com>
2019-02-13 13:04:31 -0700

    Update pwmrange.py

--- pwmrange.py
+++ pwmrange.py
@@ -10,7 +10,7 @@ GPIO.setmode (GPIO.BCM)         #we are programming the GPIO by BCM pin numbers.
 
 GPIO.setup(17,GPIO.OUT)           # initialize GPIO19 as an output.
 
-p = GPIO.PWM(17,200)          #GPIO17 as PWM output, with 100Hz frequency
+p = GPIO.PWM(17,500)          #GPIO17 as PWM output, with 100Hz frequency
 pwm = 75
 p.start(pwm)                              #generate PWM signal with 0 duty cycle
 
@@ -23,13 +23,13 @@ time.sleep(1)
 t = 3
 while t>0:                               #execute loop
     t -= 1
-    for x in range (49999):                          #execute loop for 50 times, x being incremented from 0 to 49.
-       pwm += .001
+    for x in range (4999):                          #execute loop for 50 times, x being incremented from 0 to 49.
+       pwm += .01
        p.ChangeDutyCycle(pwm)               #change duty cycle for varying the brightness of LED.
        time.sleep(0.001)                           #sleep for 10m second
 
-    for x in range (49999):                         #execute loop for 50 times, x being incremented from 0 to 49.
-        pwm -= .001
+    for x in range (4999):                         #execute loop for 50 times, x being incremented from 0 to 49.
+        pwm -= .01
         p.ChangeDutyCycle(pwm)        #change duty cycle for changing the brightness of LED.
         time.sleep(0.001)                          #sleep for 10m second
 
