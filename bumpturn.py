cdb07056b609b7335a1f6acbcea81bcc945645db
corbinvickery <45223748+corbinvickery@users.noreply.github.com>
2019-03-28 19:46:24 -0600

    Update bumpturn.py

--- bumpturn.py
+++ bumpturn.py
@@ -4,11 +4,19 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(17,GPIO.OUT)
-GPIO.setup(27,GPIO.OUT)
 
-pr = GPIO.PWM(17,250)          #GPIO17 as PWM output, with 100Hz frequency
-pl = GPIO.PWM(27,250)          #GPIO27 as PWM output, with 100Hz frequency
+GPIO.setup(4,GPIO.OUT)              #Sonalert "stone" signal
+GPIO.setup(17,GPIO.OUT)             #Driving motor Right signal
+GPIO.setup(27,GPIO.OUT)             #Driving motor Left  signal
+GPIO.setup(22,GPIO.OUT)             #Reverse trigger Right
+GPIO.setup(23,GPIO.OUT)             #Reverse trigger Left
+GPIO.setup(24,GPIO.IN)              #Bump sense Right
+GPIO.setup(25,GPIO.IN)              #Bump sense Left
+
+stone = GPIO.PWM(4,100)            #GPIO17 PWM, with 100Hz
+driver = GPIO.PWM(17,100)           #GPIO17 PWM, with 100Hz frequency used for Right motor
+drivel = GPIO.PWM(27,100)           #GPIO27 PWM, with 100Hz frequency used for Left  motor
+
 pwmr = 35.0
 pwml = 35.0
 pr.start(pwmr)
@@ -25,7 +33,7 @@ def forward():
     print ("Speed up forward")
     global pwmr
     global pwml
-    for x in range (150):                          #execute loop for 60000 times, x being incremented from 0 to 60000.
+    for x in range (150):                          #
        
        pwmr += .1
        pwml += .1
@@ -35,7 +43,7 @@ def forward():
 
 
 def fstop():
-    print ("Change from forword to stop")
+    print ("Slow down forward")
     global pwmr
     global pwml
     for x in range (150):                          #execute loop for 60000 times, x being incremented from 0 to 60000.
@@ -47,7 +55,7 @@ def fstop():
        time.sleep(0.01)                           #sleep for 10m second
 
 def rstop():
-    print ("Change from reverse to stop")
+    print ("Slow down reverse")
     global pwmr
     global pwml
     for x in range (100):                          #execute loop for 60000 times, x being incremented from 0 to 60000.
@@ -100,3 +108,8 @@ for x in range (1):
     time.sleep(1)
     turn()
     time.sleep(1)
+   # while True:
+   # input_state = GPIO.input(18)
+    #if input_state == False:
+     #  print('Button Pressed')
+      #  time.sleep(0.2)
